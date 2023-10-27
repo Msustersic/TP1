f4dec7144ba92dbe409325827cb023a2a88bd104
Matias Sustersic <msustersic@adifse.com.ar>
2023-10-27 18:44:53 -0300

    revision grupal 27-10

--- netflix-master/js/comentarios fetch.js
+++ netflix-master/js/comentarios fetch.js
@@ -1,10 +1,10 @@
-const API_URL = 'https://jsonplaceholder.typicode.com';
+const API_URL = 'https://sheetdb.io/api/v1';
 
 const HTMLResponse = document.querySelector("#api")
 
-fetch(`${API_URL}/comments`)
+fetch(`${API_URL}/p6vytybspledu`)
     .then(response => response.json())
     .then(users =>    {
-    const tpl = users.map (user => `<li>${user.email}: ${user.body}</li>`);
+    const tpl = users.map (user => `<li>${user.Nombre}: ${user.Comentario}</li>`);
     HTMLResponse.innerHTML = `<ul>${tpl}</ul>`;
 });
